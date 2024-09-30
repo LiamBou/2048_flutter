8feacf5ec8077d2ec54f16dfc2c21aa3c1b8f111
Liam Boudadi <bou.liamou@gmail.com>
2024-09-30 17:00:03 +0200

    Game over and best score when app closed added
    Game over popup
    Best score store using SharedPreferences

--- lib/models/score_model.dart
+++ lib/models/score_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ScoreModel extends ChangeNotifier {
   int _score = 0;
@@ -7,18 +8,30 @@ class ScoreModel extends ChangeNotifier {
   int get score => _score;
   int get best => _best;
 
+  ScoreModel() {
+    loadPreferences();
+  }
+
   void updateScore(int newScore) {
     _score = newScore;
     notifyListeners();
   }
 
-  void updateBest(int newBest) {
+  void updateBest(int newBest) async {
+    final prefs = await SharedPreferences.getInstance();
     _best = newBest;
+    prefs.setInt('best', _best);
     notifyListeners();
   }
 
-  void resetScore() {
+  void resetScore() async {
     _score = 0;
+    notifyListeners();
+  }
+
+  void loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    _best = prefs.getInt('best') ?? 0;
     notifyListeners();
   }
 }
